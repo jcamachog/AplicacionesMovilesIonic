c3f5f00f7d9a15a32177a68b989202acb223b933
Juan Manuel Camacho <juan.manuel.camacho.garcia.pr@gmail.com>
2020-03-22 16:11:59 -0600

    second commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
@@ -7,12 +8,36 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+  public folder: string;  
+  public name: string;
+  public age : string;
+  public sex: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, public alertController: AlertController) { }
+
+  async presentAlert() {
+
+    var str = 'Bienvenido: ' + this.name + '<br/>De sexo: ' + this.sex + '<br/>De edad: ' + this.age;
+
+    const alert = await this.alertController.create({
+      header: 'HOLA MUNDO',
+      subHeader: '',
+      message: str,
+      buttons: ['ACEPTAR']
+    });
+
+    await alert.present();
+  }
+
+  cleanFields() {   
+    this.ngOnInit();
+  }  
 
   ngOnInit() {
     this.folder = "Hola Mundo";
+    this.name = '';
+    this.age = '';
+    this.sex = '';
   }
 
 }
