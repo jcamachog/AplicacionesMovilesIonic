629532c39841df1688cf23aa9065831ef6c13acc
Juan Manuel Camacho <juan.manuel.camacho.garcia.pr@gmail.com>
2020-04-05 21:04:14 -0500

    commit accelerometer implemented

--- src/app/pages/sensors/acelerometer/acelerometer.page.ts
+++ src/app/pages/sensors/acelerometer/acelerometer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
 
 @Component({
   selector: 'app-acelerometer',
@@ -6,25 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acelerometer.page.scss'],
 })
 export class AcelerometerPage implements OnInit {
-
-  public axis_x:String;
-  public axis_y:String;
-  public axis_z:String;
+  
+  public state:boolean;
+  subscription:any;
+  public axis_x:number;
+  public axis_y:number;
+  public axis_z:number;
   public tittle:String;
   public urlBackward:String;
 
-  constructor() { }
+  constructor(private deviceMotion: DeviceMotion) { }
 
-  ngOnInit() {    
+  ngOnInit() { 
+    this.state = false;   
     this.tittle = "Sensor Acelerómetro";
     this.urlBackward = "/sensors/types";
+
+    this.deviceMotion.getCurrentAcceleration().then(
+      (acceleration: DeviceMotionAccelerationData) => console.log(acceleration.x, acceleration.y, acceleration.z),
+      (error: any) => console.log(error)
+    );
   }
 
   iniciar() {
-    
+    this.state = true;    
+    this.subscription = this.deviceMotion.watchAcceleration().subscribe((acceleration: DeviceMotionAccelerationData) => {
+      this.axis_x = acceleration.x;
+      this.axis_y = acceleration.y;
+      this.axis_z = acceleration.z;
+    });
   }
 
   detener() {
-    //this.subscription.unsubscribe();
+    this.state = false;    
+    this.subscription.unsubscribe();
+    this.axis_x = 0;
+    this.axis_y = 0;
+    this.axis_z = 0;
   }
 }
