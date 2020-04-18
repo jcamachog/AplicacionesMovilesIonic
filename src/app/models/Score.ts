6b24838390da5abbd3c91a450f0e3fe1db1f48e9
Juan Manuel Camacho <juanmanuel_camacho@hotmail.com>
2020-04-18 17:31:02 -0500

    commit score basic sql function

---
+++ src/app/models/Score.ts
@@ -0,0 +1,13 @@
+export class Score {
+    public id:number;
+    public week:string;
+    public activity:string;
+    public score:string;
+
+    constructor() {
+        this.id = null;
+        this.week = null;
+        this.activity = null;
+        this.score = null;
+    }
+}
--- src/app/services/scores.service.ts
+++ src/app/services/scores.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { Platform } from '@ionic/angular'
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScoresService {
 
-  constructor() { }
+  private database:SQLiteObject;
+  private dbList = new BehaviorSubject<boolean>(false);
+
+  constructor(private platform: Platform, private sqlite: SQLite) { 
+      this.platform.ready().then(
+        () => {
+          this.sqlite.create({
+            name : 'db_scores.db',
+            location : 'default'
+          }).then((db : SQLiteObject) => {
+            this.database = db;
+            this.createTable().then(() => {
+              this.dbList.next(true);
+            });
+          })
+        }
+      );
+  }
+
+  public getDBState() {
+    return this.dbList.asObservable();
+  }
+
+  private createTable() {    
+    const sql = 'CREATE TABLE IF NOT EXISTS scores ' +
+    '(id INTEGER PRIMARY KEY AUTOINCREMENT, week TEXT, activity TEXT, score TEXT);';
+    return this.database.executeSql(sql, []);
+  }
+
 }
